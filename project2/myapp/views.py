73c0dd4590346646ab7a8134294f2552e8b89dbd
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-06-17 07:08:05 +0100

    dynamic paths

--- project2/myapp/views.py
+++ project2/myapp/views.py
@@ -10,6 +10,8 @@ def home(request) -> HttpResponse:
     user_agent = request.META['HTTP_USER_AGENT']
     path_info = request.path_info
 
+    response = HttpResponse()
+    response.headers['Age'] = 20
     msg = f"""<br>
         <br>Path: {path}
         <br>Address: {address}
@@ -17,6 +19,19 @@ def home(request) -> HttpResponse:
         <br>Method: {method}
         <br>User_agent: {user_agent}
         <br>Path_info: {path_info}
+        <br>Response header: {response.headers}
     """
     
     return HttpResponse(msg, content_type='text/html', charset='utf-8')
+
+
+def menuitems(request, dish:str) -> HttpResponse:
+    items = {
+        'pasta': 'Pasta is a type of noodle made from a combination of wheat, water or eggs.',
+        'falafel': 'Falafel are deep fried patties or balls made from',
+        'cheesecake': 'Cheesecake is a type of dessert made with cream, soft cheese on top of cookie, pastry crust or graham cracker and fruit sauce topping.'
+    }
+
+    description = items[dish]
+    
+    return HttpResponse(f'<h2> {dish} </h2>' + description)
